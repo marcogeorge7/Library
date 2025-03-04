6e545989513c092c3851f4f833c2d872de57a34f
marcogeorge <marco.george@icloud.com>
2025-03-04 20:32:34 +0200

    books in author

--- app/Filament/Admin/Resources/BookResource.php
+++ app/Filament/Admin/Resources/BookResource.php
@@ -7,17 +7,20 @@ use App\Filament\Admin\Resources\BookResource\Pages\EditBook;
 use App\Filament\Admin\Resources\BookResource\Pages\ListBooks;
 use App\Filament\Admin\Resources\BookResource\Pages\ViewBook;
 use App\Models\Book;
-use Filament\Forms\Components\Placeholder;
+use App\Models\Publisher;
+use App\Models\Series;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Infolists\Infolist;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 
 class BookResource extends Resource
 {
@@ -35,27 +38,89 @@ class BookResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')
-                    ->required(),
 
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->label(__('Category Name'))
-                    ->searchable()
-                    ->required(),
+                Section::make('Book Main Details')
+                    ->columns(3)
+                    ->schema([
+                        TextInput::make('name')
+                            ->rules(['required', 'string'])
+                            ->disabled(fn($record) => $record ?? false)
+                            ->required(),
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->label(__('Category Name'))
+                            ->preload()
+                            ->searchable()
+                            ->required(),
 
-                Select::make('series_id')
-                    ->relationship('series', 'name')
-                    ->label(__('Series Name'))
-                    ->searchable(),
+                        Select::make('author_id')
+                            ->relationship('author', 'name')
+                            ->label(__('Author'))
+                            ->searchable(),
 
-                Placeholder::make('created_at')
-                    ->label('Created Date')
-                    ->content(fn (?Book $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+                        Checkbox::make('is_series')
+                            ->live()
+                            ->label(__('Is Series'))
+                            ->default(false)
+                            ->afterStateHydrated(fn(Checkbox $component, Book $record) => $component->state($record->hasSeries()))
+                            ->disabled(fn($record) => $record ?? false),
 
-                Placeholder::make('updated_at')
-                    ->label('Last Modified Date')
-                    ->content(fn (?Book $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+                        Select::make('series_id')
+                            ->relationship('series', 'name')
+                            ->preload()
+                            ->label(__('Series'))
+                            ->searchable()
+                            ->hintAction(
+                                Actions\Action::make('add_new_series')
+                                    ->label(__('Add Series'))
+                                    ->form([
+                                        TextInput::make('name')
+                                            ->required(),
+                                    ])->action(function (array $data) {
+                                        Series::create($data);
+                                        Notification::make()
+                                            ->title('New Series')
+                                            ->body('New Series Is Created')->send();
+                                    })
+
+                            )
+                            ->visible(fn(Get $get) => $get('is_series') === true),
+                    ]),
+                Section::make('Edition Details')
+                    ->columns()
+                    ->schema([
+                        Select::make('publisher_id')
+                            ->label(__('Publisher'))
+                            ->options(Publisher::pluck('name', 'id'))
+                            ->preload()->searchable()
+                            ->required()
+                            ->hintAction(
+                                Actions\Action::make('add_new_publisher')
+                                    ->label(__('Add Publisher'))
+                                    ->form([
+                                        TextInput::make('name')
+                                            ->required(),
+                                    ])->action(function (array $data) {
+                                        Publisher::create($data);
+                                        Notification::make()
+                                            ->title('New Publisher')
+                                            ->body('New Publisher Is Created')->send();
+                                    })
+                            ),
+
+                        TextInput::make('partCode')
+                            ->label(__('Part Code'))
+                            ->numeric()
+                            ->minValue(1)
+                            ->maxValue(100)
+                            ->required(),
+                        TextInput::make('number_of_copy')
+                            ->label(__('Number Of Copy'))
+                            ->numeric()
+                            ->minValue(1)
+                            ->default(fn() => 1)
+
+                    ])
             ]);
     }
 
